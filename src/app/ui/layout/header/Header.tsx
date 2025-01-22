4e2469c9d58c20e7b55af74039c01f7f1acdafca
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-22 18:43:26 -0300

    feat(header): estilo do header para mobile

--- src/app/ui/layout/header/Header.tsx
+++ src/app/ui/layout/header/Header.tsx
@@ -1,11 +1,19 @@
+import LogoLink from "@/app/ui/logo/LogoLink"
+
 /**
  * Cabeçalho do site.
  * @author Alexandre Raminelli
  */
 export default function Header() {
   return (
-    <header>
-      <span>Header</span>
+    <header
+      className="flex flex-row justify-between items-center
+      mx-container mt-10 
+      py-3.5 ps-6 pe-3.5
+      bg-gray-11 rounded-full border border-gray-15
+      sticky top-4"
+    >
+      <LogoLink className="h-7" />
     </header>
   )
 }

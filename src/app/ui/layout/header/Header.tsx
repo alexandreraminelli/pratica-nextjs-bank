b71204390a2104d078e7327fd44d16da2e62c50a
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-22 19:54:47 -0300

    feat(header): efeito hover no header

--- src/app/ui/layout/header/Header.tsx
+++ src/app/ui/layout/header/Header.tsx
@@ -10,10 +10,10 @@ export default function Header() {
   return (
     <header
       className="flex flex-row justify-between items-center
-      mx-container mt-10 
-      py-3.5 ps-6 pe-3.5
+      mx-container mt-10 py-3.5 ps-6 pe-3.5
       bg-gray-11 rounded-full border border-gray-15
-      sticky top-4"
+      sticky top-4 
+      hover:shadow-lg hover:border-gray-20 transition-all"
     >
       {/* Logo */}
       <LogoLink className="h-7 md:h-10 transition-all" />

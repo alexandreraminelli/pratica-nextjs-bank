9f458db43766a940f50d3dc6aa88f03393652681
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-22 20:15:12 -0300

    feat(navbar): iteração para gerar os links do navbar do header

--- src/app/ui/layout/header/navbar/Navbar.tsx
+++ src/app/ui/layout/header/navbar/Navbar.tsx
@@ -1,15 +1,41 @@
-import BurguerButton from "./BurguerButton"
+import mainNavbar from "@/data/constants/navigation/mainNavbar" // lista de links
+import Link from "next/link"
+import BurguerButton from "./BurguerButton" // ícone hambúrguer
 
 /**
  * Barra de navegação do header.
+ * @author Alexandre Raminelli
  */
 export default function Navbar() {
   return (
     <nav>
       {/* Links */}
+      <NavbarLinks />
 
       {/* Botão para mobile */}
       <BurguerButton className="md:hidden" />
     </nav>
+  )
+}
+
+/** Grupo de links do navbar. */
+function NavbarLinks() {
+  return (
+    <ul>
+      {mainNavbar.map((link, index) => (
+        <NavLink key={index} {...link} />
+      ))}
+    </ul>
+  )
+}
+
+/** Link do navbar. */
+function NavLink(
+  { href, text }: (typeof mainNavbar)[number] // props
+) {
+  return (
+    <li>
+      <Link href={href}>{text}</Link>
+    </li>
   )
 }

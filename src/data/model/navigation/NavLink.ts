61bec7772c29c9edb1f9d3ad3926fab40dd58d2f
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-22 20:06:53 -0300

    feat(navbar): modelo e constante de dados dos links do navbar principal

---
+++ src/data/model/navigation/NavLink.ts
@@ -0,0 +1,10 @@
+/**
+ * Modelo de link de navegação.
+ * @author Alexandre Raminelli
+ */
+export default interface NavLink {
+  /** Rota de endereço do link. */
+  href: string
+  /** Texto do link */
+  text: string
+}

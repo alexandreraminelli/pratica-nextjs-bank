3ae1387cba890b02892afca059f1ee1c0fb56717
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-24 18:12:45 -0300

    feat(footer): estilização inicial

--- src/app/ui/layout/footer/Footer.tsx
+++ src/app/ui/layout/footer/Footer.tsx
@@ -1,7 +1,36 @@
+import LogoLink from "@/app/ui/logo/LogoLink"
+import FooterNav from "./components/FooterNav"
+
 /**
  * Rodapé do site.
  * @author Alexandre Raminelli
  */
 export default function Footer() {
-  return <footer>Footer</footer>
+  return (
+    <footer
+      aria-label="page footer"
+      className="flex flex-col
+        bg-gray-11
+        pt-14 pb-8 px-container gap-5"
+    >
+      {/* Footer main */}
+      <FooterMain />
+
+      {/* Contato */}
+
+      {/* Redes sociais e legal */}
+    </footer>
+  )
+}
+
+/** Conteúdo principal do footer. */
+function FooterMain() {
+  return (
+    <div className="flex flex-col items-center gap-6">
+      {/* Logo */}
+      <LogoLink className="h-8" />
+      {/* Navbar */}
+      <FooterNav />
+    </div>
+  )
 }
---
+++ src/app/ui/layout/footer/components/FooterNav.tsx
@@ -0,0 +1,7 @@
+/**
+ * Menu de navegação do footer.
+ * @author Alexandre Raminelli
+ */
+export default function FooterNav() {
+  return <nav aria-label="footer navigation">Footer Navigation</nav>
+}

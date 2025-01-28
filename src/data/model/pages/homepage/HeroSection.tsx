eefad2bc9dfdeab707f6da09d98531d8b58704a6
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-28 18:33:36 -0300

    feat(hero-section): model e const do conteúdo do Hero Section

---
+++ src/data/model/pages/homepage/HeroSection.tsx
@@ -0,0 +1,20 @@
+import { BadgeProps } from "@/app/ui/components/badge/Badge" // modelo do badge
+
+/**
+ * Modelo de dados do conteúdo do Hero Section da página inicial.
+ * @author Alexandre Raminelli
+ */
+export default interface HeroSection {
+  /** Badge do Hero Section. */
+  badge: BadgeProps
+
+  /** Título de introdução */
+  title: {
+    /** Texto com coloração normal. */
+    normal: string
+    /** Texto com coloração de destaque. */
+    strong: string
+  }
+  /** Parágrafo de introdução. */
+  paragraph: string
+}

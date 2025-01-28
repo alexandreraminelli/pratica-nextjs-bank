309c06f37cc2661cf41c733199bd5797174713ad
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-28 18:45:03 -0300

    feat(hero-section): estilização do Hero Section para mobile

--- src/app/ui/pages/home/heroSection/HeroSection.tsx
+++ src/app/ui/pages/home/heroSection/HeroSection.tsx
@@ -1,7 +1,42 @@
+import Badge from "@/app/ui/components/badge/Badge"
+import heroSectionContent from "@/data/constants/pages/homepage/heroSection" // dados do Hero Section
+
+// Desencapsular dados
+const { badge, title, paragraph } = heroSectionContent
+
 /**
  * Hero Section da página inicial.
  * @author Alexandre Raminelli
  */
 export default function HeroSection() {
-  return <section>Hero Section</section>
+  return (
+    <section
+      className="flex flex-col items-center
+      px-container py-14"
+    >
+      <div className="flex flex-col gap-8">
+        {/* Header */}
+        <header className="flex flex-col gap-3 items-center">
+          {/* Badge */}
+          <Badge {...badge} />
+
+          {/* Texto */}
+          <div
+            className="flex flex-col gap-2 
+            text-center *:leading-normal"
+          >
+            {/* Título */}
+            <h2 className="font-medium text-3xl">
+              {title.normal} <strong className="font-medium text-green-60">{title.strong}</strong>
+            </h2>
+            {/* Parágrafo */}
+            <p className="font-light text-sm">{paragraph}</p>
+          </div>
+        </header>
+
+        {/* Botão */}
+        <aside className="flex justify-center">Button</aside>
+      </div>
+    </section>
+  )
 }

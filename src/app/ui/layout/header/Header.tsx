305a3e1feb3786b6aa7bacbfee17be74899f0feb
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-29 16:15:53 -0300

    chore(header): ajuste na posição sticky

--- src/app/ui/layout/header/Header.tsx
+++ src/app/ui/layout/header/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
       mx-container mt-10 py-3.5 ps-6 pe-3.5
       bg-gray-11 rounded-full border border-gray-15
       lg:*:flex-1
-      sticky top-4 
+      sticky top-2.5
       hover:shadow-lg hover:border-gray-40 transition-all"
     >
       {/* Logo */}

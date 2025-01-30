04661f3430090353ad6d9a6f6ee18ae32fcb0fb1
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-30 19:43:01 -0300

    feat(cards): responsividade dos cards01 para desktop

--- src/app/ui/components/card/Card01.tsx
+++ src/app/ui/components/card/Card01.tsx
@@ -13,17 +13,20 @@ export default function Card01({ card, className }: Card01Props) {
     <div
       aria-label="card"
       className={`flex flex-col items-center text-center
-      gap-5
+      gap-5 md:gap-6
       ${className}`}
     >
       {/* Ícone */}
       <EmphasizedIcon Icon={Icon} />
 
-      <article className="flex flex-col gap-3">
+      <article
+        className="flex flex-col 
+        gap-3 md:gap-4"
+      >
         {/* Título */}
         <h3 className="text-xl">{title}</h3>
         {/* Texto */}
-        <p className="font-light text-gray-70 text-sm leading-[1.5]">{text}</p>
+        <p className="font-light text-gray-70 max-md:text-sm leading-[1.5]">{text}</p>
       </article>
     </div>
   )
--- src/app/ui/components/card/CardGroup01.tsx
+++ src/app/ui/components/card/CardGroup01.tsx
@@ -7,17 +7,18 @@ import CardWithIcon from "@/data/model/cards/CardWithIcon"
  * @author Alexandre Raminelli
  */
 export default function CardGroup01(
-  { cardList }: CardGroup01Props // props
+  { cardList, className }: CardGroup01Props // props
 ) {
   return (
     <div
       aria-label="cards group"
-      className="flex flex-col *:flex-1
-        md:flex-row
-        max-md:divide-y md:divide-x divide-gray-15"
+      className={`flex flex-col *:flex-1
+        md:flex-row 
+        max-md:divide-y md:divide-x divide-gray-15
+        ${className}`}
     >
       {cardList.map((card, index) => (
-        <Card01 key={index} card={card} className="max-md:py-8" />
+        <Card01 key={index} card={card} className="max-md:py-8 md:px-5" />
       ))}
     </div>
   )
@@ -26,4 +27,7 @@ export default function CardGroup01(
 interface CardGroup01Props {
   /** Arrays de cards. */
   cardList: CardWithIcon[]
+
+  /** Classes personalizadas. (opcional) */
+  className?: string
 }
